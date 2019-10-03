bd69243095c0ddbd380b71c0e5a04ad44a2cd102
David Dixon <david.dixon@konsentus.com>
2019-10-03 16:33:16 +0100

    added bash to docker image to ensure this will run correctly

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM alpine/git:latest
 
 ADD entrypoint.sh /entrypoint.sh
 
+RUN apk add --no-cache bash
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 if [ -z "$GITHUB_HEAD_REF" ] || [ -z "$GITHUB_BASE_REF" ]; then
     # expected env vars dont exist, cannot continue
+    echo "Either GITHUB_HEAD_REF or GITHUB_BASE_REF are not defined. Cannot continue"
     exit 2
 fi
 
@@ -11,12 +12,13 @@ TARGET_BRANCH=$GITHUB_BASE_REF
 CHECK_TARGET_BRANCH_TMP="INPUT_$(echo $TARGET_BRANCH | awk '{print toupper($0)}')"
 CHECK_TARGET_BRANCH=(${!CHECK_TARGET_BRANCH_TMP})
 
-echo "Testing $SOURCE_BRANCH -> $TARGET_BRANCH"
+echo -n "Testing $SOURCE_BRANCH -> $TARGET_BRANCH "
 
 if [ -n "${CHECK_TARGET_BRANCH}" ]; then
     for allowed in "${CHECK_TARGET_BRANCH[@]}"; do
         if echo "$SOURCE_BRANCH" | grep -qe "$allowed"; then
             # source branch allowed to merge into target branch
+            echo "✔"
             exit 0
         fi
     done
@@ -28,10 +30,12 @@ if [ -n "${INPUT_ALL}" ]; then
     for allowed in "${INPUT_ALL_ARRAY[@]}"; do
         if echo "$SOURCE_BRANCH" | grep -qe "$allowed"; then
             # source branch allowed to merge into all branches
+            echo "✔"
             exit 0
         fi
     done
 fi
 
 # default case: source branch not permitted to merge to target branch
+echo '✘'
 exit 1
